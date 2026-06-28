f06431396f8dcb020ab5e049ba711727797aee0e
Marius <andreimarius442@yahoo.ro>
2026-06-28 19:38:37 +0300

    tried to implement metadata read

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,14 +1,41 @@
-// Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+use serde::Serialize;
+use lofty::probe::Probe;
+use lofty::tag::Accessor;
+
+#[derive(Serialize)]
+pub struct SongMetadata {
+    title: Option<String>,
+    artist: Option<String>,
+    album: Option<String>,
+
+}
+
 #[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+fn read_metadata(path: String) -> Result<SongMetadata, String> {
+    let tagged_file = Probe::open(&path)
+        .map_err(|e| e.to_string())?
+        .read()
+        .map_err(|e| e.to_string())?;
+
+    let tag = match tagged_file.primary_tag() {
+        Some(primary_tag) => Some(primary_tag),
+        None => tagged_file.first_tag(),
+    };
+
+    if let Some(tag) = tag{
+        metadata.title = tag.title().map(|s| s.into_owned());
+        metadata.artist = tag.artist().map(|s| s.into_owned());
+        metadata.album = tag.album().map(|s| s.into_owned());
+    }
+
+    Ok(metadata)
 }
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
-        .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![greet])
+        .plugin(tauri_plugin_shell::init())
+        .invoke_handler(tauri::generate_handler![read_metadata]) 
         .run(tauri::generate_context!())
-        .expect("error while running tauri application");
+        .expect("eroare la pornirea aplicatiei tauri");
 }
